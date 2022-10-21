819bf7928836b7488a33c2f7a556227b50c345bd
Renae Myers <renae.myers@erg.com>
2022-10-21 13:02:32 -0400

    final files only

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-import naicsCodes from "../data/naics.json" assert { type: "json" };
-import states from "../data/states.json" assert { type: "json" };
-import exempt from "../data/exempt.json" assert { type: "json" };
+const naicsUrl = "../oshareport/data/naics.json";
+const statesUrl = "../oshareport/data/states.json";
+const exemptUrl = "../oshareport/data/exempt.json";
 
 const DEBOUNCE_TIMEOUT_MS = 100;
 
@@ -9,14 +9,74 @@ const resultsList = document.querySelector("#autocomplete-results");
 const dropdownArrow = document.querySelector(".autocomplete__dropdown-arrow");
 const comboBox = document.querySelector(".autocomplete__container");
 
+let naicsCodes;
+let states;
+let exempt;
+
 let currentListItemFocused = -1;
 let selectedNAICS = "";
-let filteredResults = [...naicsCodes.NAICS];
 let isDropDownOpen = false;
 
+let filteredResults;
+
+
 window.onload = function() { 
-	document.querySelector("#results").style.display = "none";
+  document.querySelector("#results").style.display = "none";
 }
+
+fetch(naicsUrl)
+  .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.warn('Looks like there was a problem importing naicsCodes. Status Code: ' + 
+          response.status);  
+        return;  
+      }
+      response.json().then(function(data) {  
+        naicsCodes = data;
+        filteredResults = [...naicsCodes.NAICS];
+        setResults(filteredResults);
+      });  
+    }  
+  )  
+  .catch(function(err) {  
+    console.error('naicsCodes Fetch Error -', err);  
+  });  
+ 
+fetch(statesUrl)
+  .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.warn('Looks like there was a problem importing naicsCodes. Status Code: ' + 
+          response.status);  
+        return;  
+      }
+      response.json().then(function(data) {  
+        states = data;
+      });  
+    }  
+  )  
+  .catch(function(err) {  
+    console.error('States Fetch Error -', err);  
+  });
+
+fetch(exemptUrl)
+  .then(  
+    function(response) {  
+      if (response.status !== 200) {  
+        console.warn('Looks like there was a problem importing exempt states. Status Code: ' + 
+          response.status);  
+        return;  
+      }
+      response.json().then(function(data) {  
+        exempt = data;
+      });  
+    }  
+  )  
+  .catch(function(err) {  
+    console.error('Exempt States Fetch Error -', err);  
+});
+
 
 function openDropdown() {
   isDropDownOpen = true;
@@ -158,8 +218,6 @@ function handleKeyboardEvents(event) {
 input.addEventListener("keydown", handleKeyboardEvents);
 resultsList.addEventListener("keydown", handleKeyboardEvents);
 
-setResults(naicsCodes.NAICS);
-
 let bounce = undefined;
 function debounce(callback) {
   clearTimeout(bounce);
@@ -188,87 +246,87 @@ input.addEventListener("input", (event) => {
 });
 
 function getStateName(statecode) {
-	let obj = states;
-	let result = obj.States.filter(e => e.code == statecode);
-	return result[0].name;
+  let obj = states;
+  let result = obj.States.filter(e => e.code == statecode);
+  return result[0].name;
 }
 
 function getNaicsInfo(naics) {
-	let obj = naicsCodes;
-	let result = obj.NAICS.filter(e => e.NAICSCode == naics);
-	return result[0];
+  let obj = naicsCodes;
+  let result = obj.NAICS.filter(e => e.NAICSCode == naics);
+  return result[0];
 }
 
 document.querySelector("#reset").addEventListener("click", function() {
-	location.reload();
+  location.reload();
 });
 
 document.querySelector("#form1").addEventListener("submit", function (event) {
-	event.preventDefault()
+  event.preventDefault()
 
-	const state = document.querySelector("#state").value;
-	const employment = document.querySelector("#employment").value;
-	const government = document.querySelector('input[name=government]:checked').value;
-	const naicsInfo = getNaicsInfo(selectedNAICS);
+  const state = document.querySelector("#state").value;
+  const employment = document.querySelector("#employment").value;
+  const government = document.querySelector('input[name=government]:checked').value;
+  const naicsInfo = getNaicsInfo(selectedNAICS);
   const exemptPrivStates = exempt.exemptPrivStates
   const exemptStates = exempt.exemptStates
 
-	let resultsStyle = "required";
+  let resultsStyle = "required";
   let governmentType = "Non-government";
 
-	if (government == 'federal') {
-		resultsStyle = "exempt";
+  if (government == 'federal') {
+    resultsStyle = "exempt";
     governmentType = 'Federal'
-		document.querySelector("#fed-exempt").style.display = "list-item";
-	} else if (exemptPrivStates.includes(state) && (government == "nongovernment" || government == "statelocal")) {
-		resultsStyle = "possible";
+    document.querySelector("#fed-exempt").style.display = "list-item";
+  } else if (exemptPrivStates.includes(state) && (government == "nongovernment" || government == "statelocal")) {
+    resultsStyle = "possible";
     if (government == "statelocal") {
        governmentType = 'State or Local';
     }
-		document.querySelector('#possible-exempt').style.display = "list-item";
-	} else if (exemptStates.includes(state) && (government == "statelocal")) {
-		resultsStyle = "possible";
+    document.querySelector('#possible-exempt').style.display = "list-item";
+  } else if (exemptStates.includes(state) && (government == "statelocal")) {
+    resultsStyle = "possible";
     governmentType = 'State or Local';
-		document.querySelector("#possible-exempt").style.display = "list-item";
-	} else if (government == "statelocal") { 
-		resultsStyle = "exempt";
+    document.querySelector("#possible-exempt").style.display = "list-item";
+  } else if (government == "statelocal") { 
+    resultsStyle = "exempt";
     governmentType = 'State or Local';
-		document.querySelector("#state-govt-exempt").style.display = "list-item";
-	} else {
-		if (naicsInfo.RKExempt == "TRUE") {
-			resultsStyle = "exempt";
-			document.querySelector("#rk-exempt").style.display = "list-item";
-		} else if (naicsInfo.NotOSHAJurisdiction == "TRUE") {
-			resultsStyle = "exempt";
-			document.querySelector("#naics-exempt").style.display = "list-item";
-		}
-		if (naicsInfo.Employees20 == "TRUE" && employment < 20) {
-			resultsStyle = "exempt";
-			document.querySelector("#employment-exempt").style.display = "list-item";
-		} else if (naicsInfo.Employees250 == "TRUE" && employment < 250) {
-			resultsStyle = "exempt";
-			document.querySelector("#employment250-exempt").style.display = "list-item";
-		}
-	}
+    document.querySelector("#state-govt-exempt").style.display = "list-item";
+  } else {
+    if (naicsInfo.RKExempt == "TRUE") {
+      resultsStyle = "exempt";
+      document.querySelector("#rk-exempt").style.display = "list-item";
+    } else if (naicsInfo.NotOSHAJurisdiction == "TRUE") {
+      resultsStyle = "exempt";
+      document.querySelector("#naics-exempt").style.display = "list-item";
+    }
+    if (naicsInfo.Employees20 == "TRUE" && employment < 20) {
+      resultsStyle = "exempt";
+      document.querySelector("#employment-exempt").style.display = "list-item";
+    } else if (naicsInfo.Employees250 == "TRUE" && employment < 250) {
+      resultsStyle = "exempt";
+      document.querySelector("#employment250-exempt").style.display = "list-item";
+    }
+  }
 
-	document.querySelector("#results-state").innerHTML = 
-		document.querySelector("#results-state").innerHTML.replace("SS", getStateName(state)); 
-	document.querySelector("#results-employment").innerHTML = 
-		document.querySelector("#results-employment").innerHTML
-			.replace("EEEE", Number(employment).toLocaleString());
+  document.querySelector("#results-state").innerHTML = 
+    document.querySelector("#results-state").innerHTML.replace("SS", getStateName(state)); 
+  document.querySelector("#results-employment").innerHTML = 
+    document.querySelector("#results-employment").innerHTML
+      .replace("EEEE", Number(employment).toLocaleString());
   document.querySelector("#results-government").innerHTML = 
     document.querySelector("#results-government").innerHTML.replace("GG", governmentType);      
-	document.querySelector("#results-naics").innerHTML = 
-		document.querySelector("#results-naics").innerHTML
-			.replace("NNNN", naicsInfo.NAICSCode + ": " + naicsInfo.NAICSTitle); 
+  document.querySelector("#results-naics").innerHTML = 
+    document.querySelector("#results-naics").innerHTML
+      .replace("NNNN", naicsInfo.NAICSCode + ": " + naicsInfo.NAICSTitle); 
 
   document.querySelector(".results_ul").style.display = "list-item";  
   let elList = document.querySelectorAll("." + resultsStyle);
   elList.forEach(el => el.style.display = "list-item");
 
-	document.querySelector("#intro").style.display = "none";	
-	document.querySelector("#data-entry").style.display = "none";		
-	document.querySelector("#results").style.display = "block";
+  document.querySelector("#intro").style.display = "none";  
+  document.querySelector("#data-entry").style.display = "none";   
+  document.querySelector("#results").style.display = "block";
 });
 
 
